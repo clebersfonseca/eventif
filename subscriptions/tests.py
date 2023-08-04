e7369a6ca19e1319ba40cf94ec32a30839b3f440
Cleber Fonseca <profcleberfonseca@gmail.com>
2023-08-04 12:06:37 -0300

    Verificando os dados enviados pelo email

--- subscriptions/tests.py
+++ subscriptions/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from subscriptions.forms import SubscriptionForm
+from django.core import mail
 
 class SubscribeTest(TestCase):
     def setUp(self):
@@ -33,3 +34,40 @@ class SubscribeTest(TestCase):
     def test_form_has_fields(self):
         form = self.response.context['form']
         self.assertSequenceEqual(['name', 'cpf', 'email', 'phone'], list(form.fields)) 
+
+class SubscribeTestPost(TestCase):
+    def setUp(self):
+        data = dict(name="Cleber Fonseca", 
+                    cpf="12345678901", 
+                    email="profcleberfonseca@gmail.com",
+                    phone="53-91234-5678")
+        
+        self.response = self.client.post('/inscricao/', data)
+
+    def test_post(self):
+        self.assertEqual(302, self.response.status_code)
+
+    def test_send_subscribe_email(self):
+        self.assertEqual(1, len(mail.outbox))
+
+    def test_subscription_email_subject(self):
+        email = mail.outbox[0]
+        expect = "Confirmação de inscrição"
+        self.assertEqual(expect, email.subject)
+
+    def test_subscription_email_sender(self):
+        email = mail.outbox[0]
+        expect = "contato@eventif.com.br"
+        self.assertEqual(expect, email.from_email)
+
+    def test_subscription_email_to(self):
+        email = mail.outbox[0]
+        expect = ['contato@eventif.com.br', 'profcleberfonseca@gmail.com']
+        self.assertEqual(expect, email.to)
+
+    def test_subscription_email_body(self):
+        email = mail.outbox[0]
+        self.assertIn('Cleber Fonseca', email.body)
+        self.assertIn('12345678901', email.body)
+        self.assertIn('profcleberfonseca@gmail.com', email.body)
+        self.assertIn('53-91234-5678', email.body)
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,7 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from subscriptions.forms import SubscriptionForm
+from django.core import mail
+from django.template.loader import render_to_string
 
 def subscribe(request):
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+        form.full_clean()
+        body = render_to_string('subscriptions/subscription_email.txt',form.cleaned_data)
+        mail.send_mail('Confirmação de inscrição',
+                        body,
+                        'contato@eventif.com.br',
+                        ['contato@eventif.com.br', form.cleaned_data['email']])
+        return HttpResponseRedirect('/inscricao/')
     context = {"form": SubscriptionForm()}
     return render(request, 'subscriptions/subscription_form.html', context)
