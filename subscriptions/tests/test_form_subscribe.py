a6cf0025c70f0f237efaf258c913619e2ab3583b
Cleber Fonseca <profcleberfonseca@gmail.com>
2023-10-27 09:14:06 -0300

    Validação CPF

--- subscriptions/tests/test_form_subscribe.py
+++ subscriptions/tests/test_form_subscribe.py
@@ -4,8 +4,36 @@ from subscriptions.forms import SubscriptionForm
 
 
 class subscriptionFormTest(TestCase):
-    def setUp(self):
-        self.form = SubscriptionForm()
-
     def test_form_has_fields(self):
-        self.assertSequenceEqual(['name', 'cpf', 'email', 'phone'], list(self.form.fields)) 
+        self.form = SubscriptionForm()
+        self.assertSequenceEqual(
+            ['name', 'cpf', 'email', 'phone'], list(self.form.fields))
+
+    def test_cpf_has_digit(self):
+        form = self.make_validated_form(cpf='ABCD5678901')
+        self.assertFormErrorCode(form, 'cpf', 'digits')
+
+    def test_cpf_has_11_digits(self):
+        form = self.make_validated_form(cpf='123456')
+        self.assertFormErrorCode(form, 'cpf', 'length')
+
+    def assertFormErrorCode(self, form, field, code):
+        errors = form.errors.as_data()
+        error_list = errors[field]
+        exception = error_list[0]
+        self.assertEqual(code, exception.code)
+
+    def assertFormErrorMessage(self, form, field, msg):
+        errors = form.errors
+        error_list = errors[field]
+        self.assertListEqual([msg], error_list)
+
+    def make_validated_form(self, **kwargs):
+        valid = dict(name='Cleber Fonseca', cpf='12345678901',
+                     email='profcleberfonseca@gmail.com', phone='53-912345678')
+        data = dict(valid, **kwargs)
+        form = SubscriptionForm(data)
+        form.is_valid()
+        return form
+
+    # 12345678901 123456
