c5935c3f581363d4017e3925dca02a5bb00dc66b
Cleber Fonseca <profcleberfonseca@gmail.com>
2024-11-11 17:11:16 -0300

    Admin Magic

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.utils.timezone import now
+from subscriptions.models import Subscription
 
-# Register your models here.
+
+class SubscriptionModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'cpf',
+                    'created_at', 'subscribed_today')
+    date_hierarchy = 'created_at'
+    search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
+    list_filter = ('created_at',)
+
+    def subscribed_today(self, obj):
+        return obj.created_at.date() == now().date()
+
+    subscribed_today.short_description = 'Inscrito hoje?'
+    subscribed_today.boolean = True
+
+
+admin.site.register(Subscription, SubscriptionModelAdmin)
--- subscriptions/apps.py
+++ subscriptions/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SubscriptionsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'subscriptions'
+    verbose_name = "Controle de Participantes"
