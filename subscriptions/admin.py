6a5f713d4a5da810e3d4c4bcff28d141cd2d3bb3
Cleber Fonseca <profcleberfonseca@gmail.com>
2024-11-25 15:14:58 -0300

    Marcar como pago

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -9,12 +9,25 @@ class SubscriptionModelAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at'
     search_fields = ('name', 'email', 'phone', 'cpf', 'created_at')
     list_filter = ('paid', 'created_at')
+    actions = ['mark_as_paid',]
 
     def subscribed_today(self, obj):
         return obj.created_at.date() == now().date()
 
     subscribed_today.short_description = 'Inscrito hoje?'
     subscribed_today.boolean = True
+
+    def mark_as_paid(self, request, queryset):
+        count = queryset.update(paid=True)
+
+        if count == 1:
+            msg = "{} inscrição foi marcada como paga."
+        else:
+            msg = "{} inscrições foram marcadas como pagas"
+
+        self.message_user(request, msg.format(count))
+
+    mark_as_paid.short_description = 'Marcar como pago'
 
 
 admin.site.register(Subscription, SubscriptionModelAdmin)
