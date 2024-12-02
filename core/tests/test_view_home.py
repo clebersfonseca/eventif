9f82454bea4f71264f377bd078ad9b4ad459a8bd
Cleber Fonseca <profcleberfonseca@gmail.com>
2024-12-02 15:19:05 -0300

    keynote speakers

--- core/tests/test_view_home.py
+++ core/tests/test_view_home.py
@@ -18,3 +18,18 @@ class HomeTest(TestCase):
     def test_link_subscription(self):
         self.assertContains(
             self.response, 'href="{}"'.format(r('subscriptions:new')))
+
+    def test_speakers(self):
+        contents = [
+            'Grace Hopper',
+            'https://abre.ai/hopper-pic',
+            'Alan Turing',
+            'https://abre.ai/turing-pic'
+        ]
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+
+    def test_speakers_link(self):
+        expected = 'href="{}#speakers"'.format(r('home'))
+        self.assertContains(self.response, expected)
